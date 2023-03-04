8e70ac303da1b1a630b585790e8abf3e555e2946
ieatyoursushi <69708951+ieatyoursushi@users.noreply.github.com>
2023-03-04 12:48:54 -0800

    added word database, a collection of word files.

--- scripts/generateWords.js
+++ scripts/generateWords.js
@@ -1,25 +1,29 @@
 const textArea = document.getElementById("textArea");
 export {textArea};
-const words = [
-    'the', 'of', 'and', 'a', 'to', 'in', 'that', 'it', 'with', 'for',
-    'was', 'on', 'is', 'as', 'by', 'at', 'an', 'be', 'this', 'which',
-    'or', 'from', 'but', 'not', 'are', 'his', 'they', 'her', 'she', 'been',
-    'had', 'their', 'there', 'can', 'all', 'we', 'if', 'more', 'when', 'will',
-    'would', 'who', 'so', 'no', 'them', 'out', 'up', 'into', 'your', 'has',
-    'some', 'do', 'could', 'my', 'than', 'other', 'its', 'about', 'also', 'then',
-    'only', 'time', 'these', 'may', 'like', 'any', 'such', 'now', 'over', 'our',
-    'even', 'most', 'me', 'after', 'made', 'many', 'before', 'must', 'through', 'back',
-    'years', 'where', 'much', 'your', 'way', 'well', 'down', 'should', 'because', 'each'
-  ];
-for(let i = 0; i < 100; i++) {
-    let word = document.createElement("span");
-    word.id = (i + 1);
-    //word.classList.add("");
-    word.className="";
-    word.innerText = words[randomRange(0, words.length-1)];
-    textArea.appendChild(word);
-    textArea.appendChild(document.createTextNode(' '));
+ 
+function fetchWordsDB() {
+    return new Promise((resolve, reject) => {
+        fetch('WordDatabases/top_100_eng.txt')
+        .then(res => res.text()).then(data => {
+            resolve(data);
+        })
+    })
 }
-function randomRange(min, max) {
-    return Math.floor(Math.random() * (max - min)) + min;
-}
+fetchWordsDB().then(wordsFile => {
+    let words = wordsFile.split("\n").map(line => line.replace('\r', ''));
+    console.log(words);
+    for(let i = 0; i < 15; i++) {
+        let word = document.createElement("span");
+        word.id = (i + 1);
+        //word.classList.add("");
+        word.className="";
+        word.innerText = words[randomRange(0, words.length-1)];
+        textArea.appendChild(word);
+        textArea.appendChild(document.createTextNode(' '));
+    }
+    function randomRange(min, max) {
+        return Math.floor(Math.random() * (max - min)) + min;
+    }
+})
+
+ 
--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,8 @@
 import {textArea} from "../scripts/generateWords.js";
 const userInp = document.getElementById("userInput");
+const timer = document.getElementById("timer");
 let gameRunning = false;
+let seconds = 0;
 function startGame() {
     return new Promise((resolve, reject) => {
         setInterval(() => {
@@ -14,28 +16,64 @@ startGame().then(status => {
     gameRunning = status;
     console.log(`Game started: ${gameRunning}`);
     if(gameRunning) {
-        textArea.children[0].style.background = "#dddddd"
+        startCounter().then(seconds => {
+            console.log(seconds);
+            displayWPM(seconds);
+        });
+        textArea.children[0].style.background = "#dddddd";
         checkTyping();
     }
 
 }).catch(err => {
     console.log(err);
 })
+//wrap in ded. class or different file
+let totalKeyStrokes = 1;
+function displayWPM(seconds) {
+    //          5 chars = word ------ 60 seconds = minute
+    let wpm = (totalCharacters() / 5)/(seconds/60);
+    console.log(totalCharacters() + " " + totalKeyStrokes);
+    document.getElementById("wpmDisplay").innerHTML = wpm.toFixed(2) + " WPM " + (totalCharacters() / totalKeyStrokes).toFixed(2)* 100 + "%";
+}
+function totalCharacters() {
+    let sum = 0;
+    for(let i = 0; i < textArea.children.length; i++) {
+        sum += textArea.children[i].innerHTML.length;
+    }
+    return sum;
+} 
+
+function startCounter() {
+    return new Promise((resolve, reject) => {
+        let interval = setInterval(() => {
+            if(gameRunning) {
+            seconds += .01;
+            timer.innerHTML = `${seconds.toFixed(2)}`;
+            } else {
+                resolve(seconds.toFixed(2));
+                clearInterval(interval);
+            }
+        }, 10)
+    })
+}
 
 let i = 0;
 function checkTyping() {
     document.addEventListener('keydown', function(event) {
+        if(gameRunning && event.code !== 'Space' && event.code !== "Backspace") {
+            totalKeyStrokes++;
+        }
         //same if statement but with a clause gaurd, hope i remember demorgans laws
         if (!(event.code === "Space" && userInp.value.trim() !== 0 && i < textArea.children.length && gameRunning)) {
             return;
         }
+ 
         compareUserInputToCurrentIndexSpanTagsInnerHTML();
         userInp.value = "";
     })
 }
 function compareUserInputToCurrentIndexSpanTagsInnerHTML() {
     let currentWord = textArea.children[i].innerHTML;
-    console.log("space bar clicked");
     if (userInp.value.trim() === textArea.children[i].innerHTML) {
         i++;
         console.log(i);
@@ -54,7 +92,7 @@ function compareUserInputToCurrentIndexSpanTagsInnerHTML() {
     }
 }
 function autoScroll(i) {
-    const parentHeight = textArea.offsetHeight;
+    const parentHeight = textArea.offsetHeight - 52;
     let currentElement = textArea.children[i];
     if(currentElement.offsetTop > parentHeight) {
         textArea.scrollTop = currentElement.offsetTop - parentHeight;
