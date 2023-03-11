dc6be89cadd50f5dd44b0da3526b911406e94345
ieatyoursushi <69708951+ieatyoursushi@users.noreply.github.com>
2023-03-11 10:22:18 -0800

    file organize

--- scripts/generateWords.js
+++ scripts/generateWords.js
@@ -1,29 +1,56 @@
 const textArea = document.getElementById("textArea");
 export {textArea};
- 
-function fetchWordsDB() {
+let amountOfWords = 50;
+export {amountOfWords}
+let urlPath = '/' + location.href.split("#")[1];
+function fetchWordsDB(path) {
     return new Promise((resolve, reject) => {
-        fetch('WordDatabases/top_100_eng.txt')
+        fetch('WordDatabases/' + path)
         .then(res => res.text()).then(data => {
             resolve(data);
         })
     })
 }
-fetchWordsDB().then(wordsFile => {
-    let words = wordsFile.split("\n").map(line => line.replace('\r', ''));
-    console.log(words);
-    for(let i = 0; i < 15; i++) {
-        let word = document.createElement("span");
-        word.id = (i + 1);
-        //word.classList.add("");
-        word.className="";
-        word.innerText = words[randomRange(0, words.length-1)];
-        textArea.appendChild(word);
-        textArea.appendChild(document.createTextNode(' '));
-    }
-    function randomRange(min, max) {
-        return Math.floor(Math.random() * (max - min)) + min;
-    }
+function displayWords(path) {
+    fetchWordsDB(path).then(wordsFile => {
+        let words = wordsFile.split("\n").map(line => line.replace('\r', ''));
+        console.log(words);
+        for (let i = 0; i < amountOfWords; i++) {
+            let word = document.createElement("span");
+            word.id = (i + 1);
+            //word.classList.add("");
+            word.className = "";
+            word.innerText = words[randomRange(0, words.length - 1)];
+            textArea.appendChild(word);
+            textArea.appendChild(document.createTextNode(' '));
+        }
+
+        function randomRange(min, max) {
+            return Math.floor(Math.random() * (max - min)) + min;
+        }
+    })
+}
+console.log(urlPath);
+console.log(urlPath === '/#top100eng')
+if(urlPath === "/top100eng") {
+    displayWords('English/top_100.txt');
+} else if (urlPath === "/top1000eng") {
+    displayWords('English/top_1000.txt');
+} else if (urlPath === "/top_100esp") {
+    displayWords("Spanish/top_100.txt");
+}
+document.getElementById("1000_eng").addEventListener('click', function() {
+    location.href = '/#top1000eng';
+    window.location.reload();
 })
+document.getElementById("100_eng").addEventListener('click', function() {
+    location.href = '/#top100eng';
+    window.location.reload();
+})
+document.getElementById("100_esp").addEventListener('click', function() {
+    location.href= '/#top_100esp';
+    window.location.reload();
+})
+ 
 
  
--- scripts/typing.js
+++ scripts/typing.js
@@ -1,8 +1,10 @@
-import {textArea} from "../scripts/generateWords.js";
+import {textArea} from "./generateWords.js";
+import { amountOfWords } from "./generateWords.js";
 const userInp = document.getElementById("userInput");
 const timer = document.getElementById("timer");
 let gameRunning = false;
 let seconds = 0;
+let totalKeyStrokes = 1;
 function startGame() {
     return new Promise((resolve, reject) => {
         setInterval(() => {
@@ -27,22 +29,6 @@ startGame().then(status => {
 }).catch(err => {
     console.log(err);
 })
-//wrap in ded. class or different file
-let totalKeyStrokes = 1;
-function displayWPM(seconds) {
-    //          5 chars = word ------ 60 seconds = minute
-    let wpm = (totalCharacters() / 5)/(seconds/60);
-    console.log(totalCharacters() + " " + totalKeyStrokes);
-    document.getElementById("wpmDisplay").innerHTML = wpm.toFixed(2) + " WPM " + (totalCharacters() / totalKeyStrokes).toFixed(2)* 100 + "%";
-}
-function totalCharacters() {
-    let sum = 0;
-    for(let i = 0; i < textArea.children.length; i++) {
-        sum += textArea.children[i].innerHTML.length;
-    }
-    return sum;
-} 
-
 function startCounter() {
     return new Promise((resolve, reject) => {
         let interval = setInterval(() => {
@@ -56,14 +42,12 @@ function startCounter() {
         }, 10)
     })
 }
-
 let i = 0;
 function checkTyping() {
     document.addEventListener('keydown', function(event) {
-        if(gameRunning && event.code !== 'Space' && event.code !== "Backspace") {
+        if(gameRunning && event.code !== 'Space' && event.key.length === 1 && /[a-z0-9\s\.,-]/i.test(event.key)) {
             totalKeyStrokes++;
         }
-        //same if statement but with a clause gaurd, hope i remember demorgans laws
         if (!(event.code === "Space" && userInp.value.trim() !== 0 && i < textArea.children.length && gameRunning)) {
             return;
         }
@@ -97,4 +81,30 @@ function autoScroll(i) {
     if(currentElement.offsetTop > parentHeight) {
         textArea.scrollTop = currentElement.offsetTop - parentHeight;
     }
+}
+
+//wrap in ded. class or different file
+function displayWPM(seconds) {
+    //          5 chars = word ------ 60 seconds = minute
+    let wpm = (totalCharactersRaw() / 5)/(seconds/60);
+    let totalKeyStrokesRaw = totalKeyStrokes + (amountOfWords - 1)
+    let accuracy = ((totalCharactersRaw() / totalKeyStrokesRaw).toFixed(4)* 100).toFixed(2);
+    document.getElementById("wpmDisplay").innerHTML = `${wpm.toFixed(2)} WPM ${accuracy}%`;
+    console.log(totalCharacters() + " " + totalKeyStrokes);
+    console.log(totalCharactersRaw() + " " + totalKeyStrokesRaw);
+}   
+function totalCharacters() {
+    let sum = 0;
+    for(let i = 0; i < textArea.children.length; i++) {
+        sum += textArea.children[i].innerHTML.length;
+    }
+    return sum;
+} 
+function totalCharactersRaw() {
+    let sum = 0;
+    for(let i = 0; i < textArea.children.length; i++) {
+        sum += textArea.children[i].innerHTML.length;
+        sum++;
+    }
+    return sum-1;
 }
